f407aaa36f51c4551c31dd2dbabbf359a307fd99
Nikola <nikj1705@gmail.com>
2020-12-22 14:46:04 +0200

    removed hard-coded filenames

--- bio_main.py
+++ bio_main.py
@@ -8,42 +8,67 @@ if __name__ == '__main__':
     print("Hello world")
     print("Bioinformatics 2020/2021")
 
+    # Open Sausage File
+    print("Enter Sausage File Name")
+    print("Example: SausageTest.fasta")
+    user_sausage_filename = input(">>> ")
+
     # Check if file is available
     try:
-        test = open('SausageTest.fasta', 'r')
+        test = open(user_sausage_filename, 'r')
     except FileNotFoundError:
-        print("Can't open the SausageTest.fasta file!")
+        print(f"Can't open {user_sausage_filename}!")
         input("Press any key to continue")
+        quit()
     else:
         test.close()
 
+    # Split Sausage File to Sequences
     # get sausage data, divide in windows with length 51
     sausage_length = 51
     sausage_data = read_sausage('SausageTest.fasta', sausage_length)
+    A_seq, G_seq, C_seq, T_seq = organize_sausage(sausage_data)
 
-    # split Genome data in multiple files
-    user_split_choice = input("Would you like to split the big genome file in multiple smaller files? [y/n] >>> ")
+    # Open and Split Genome File
+    print(
+        "Would you like to split a big genome file (from the database, not necessarily a split) in multiple smaller "
+        "files? [y/n]")
+    user_split_choice = input(">>> ")
     user_parts = ' '
     if user_split_choice in ['yes', 'YES', 'y']:
+        # get name of file
+        print("Enter Genome File Name")
+        user_genome_filename = input(">>> ")
+        # check if such file exists
+        try:
+            test = open(user_genome_filename, 'r')
+        except FileNotFoundError:
+            print(f"Can't open {user_genome_filename}!")
+            input("Press any key to continue")
+            quit()
+        else:
+            test.close()
+
+        # get number of parts
         while not user_parts.isdigit():
             user_parts = input("Enter number of parts: ")
         else:
-            split_genome('pig_1.fasta', user_parts)
+            user_parts = int(user_parts)
+            split_genome(user_genome_filename, user_parts)
     else:
         pass
 
-    A_seq, G_seq, C_seq, T_seq = organize_sausage(sausage_data)
-
-    print("Enter Genome File Name")
-    print("Example: genome_split_8.txt")
-    genome_filename = input(">>> ")
+    # Compare Sequences
+    print("Enter Genome Split File Name")
+    print("Example: genome_split_6_20.txt")
+    user_split_filename = input(">>> ")
 
     # Check if file is available
     try:
-        test = open(genome_filename, 'r')
+        test = open(user_split_filename, 'r')
     except FileNotFoundError:
-        print(genome_filename, "not found!")
+        print(user_split_filename, "not found!")
         input("Press any key to continue")
     else:
         test.close()
-        compare_genome(genome_filename, A_seq, G_seq, C_seq, T_seq, sausage_length)
+        compare_genome(user_split_filename, A_seq, G_seq, C_seq, T_seq, sausage_length)
